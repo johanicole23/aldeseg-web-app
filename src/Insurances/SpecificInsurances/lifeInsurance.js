ac51c055b0bfc595ed5db68094446fd380ae1f01
EzequielCastilloH <emcastillo2@espe.edu.ec>
2024-06-17 23:37:30 -0500

    New components added

--- src/Insurances/SpecificInsurances/lifeInsurance.js
+++ src/Insurances/SpecificInsurances/lifeInsurance.js
@@ -4,10 +4,13 @@ import text from '../../Styles/text';
 import myTheme from '../../Styles/myTheme';
 import NavBar from '../../ExtraComponents/navBar';
 import Footer from '../../ExtraComponents/footer';
+import box from '../../Styles/box';
 import StyledBreadcrumb from '../../ExtraComponents/styledBreadcrumb';
 import lifeImg from '../../assets/images/seguro_vida/slide3.jpg';
 import ImageWithDescription from '../../ExtraComponents/imageWithDescription';
-import {generalCardsData} from '../../assets/data';
+import { generalCardsData } from '../../assets/data';
+import { homeCircleIcons } from '../../assets/data';
+import CircleComponent from '../../ExtraComponents/circleComponent';
 
 function lifeInsurance() {
 
@@ -31,7 +34,8 @@ function lifeInsurance() {
                     height: '220px'
                 },
             }}>
-                <Box sx={{ mt: '5%', width: '40%', ml: '5%',
+                <Box sx={{
+                    mt: '5%', width: '40%', ml: '5%',
                     '@media screen and (max-width: 600px)': {
                         width: '90%',
                         ml: '5%',
@@ -40,7 +44,7 @@ function lifeInsurance() {
                     '@media screen and (min-width: 700px) and (max-width: 840px)': {
                         mt: '8%'
                     }
-                 }}>
+                }}>
                     <Typography sx={[text.textH1, {
                         '@media screen and (max-width: 600px)': {
                             fontSize: '2rem'
@@ -65,22 +69,37 @@ function lifeInsurance() {
                 </Box>
             </Box>
             <Divider />
+
             <Box sx={{ mt: '2%', ml: '2%' }}>
                 <StyledBreadcrumb component="a" href="/" label="Inicio" />
                 {' > '}
                 <StyledBreadcrumb component="a" href="/seguros" label="Seguros" />
                 {' > '}
-                <StyledBreadcrumb component="a" href="#" label="Seguro de Vida" />
+                <StyledBreadcrumb component="a" href="#" label="Seguros de Vida" />
             </Box>
-            <Box alignItems='center' justifyContent={'center'} sx={{ mt: '5%', width: '90%', marginLeft: '5%' }}>
+            <Box alignItems='center' justifyContent={'center'} sx={{ width: '90%', marginLeft: '5%' }}>
                 <Typography sx={[text.textH1, { textAlign: 'center' }]}>
                     Seguros de Vida
                 </Typography>
-                <br></br>
+                <Box sx={box.boxFormatGeneralColumn} >
+                    <CircleComponent data={homeCircleIcons} />
+                </Box>
+                <Divider/>
+            </Box>
+
+            <Typography sx={[text.textH2, { mt: '3%', ml: '5%', mr: '5%' }]}>
+                ¿Qué es un seguro de vida?
+            </Typography>
+            <ImageWithDescription imgUrlFirst={generalCardsData[0].imgUrl} descriptionFirst={generalCardsData[0].description}
+                imgUrlSecond={generalCardsData[1].imgUrl} descriptionSecond={generalCardsData[1].description} />
+            <Box alignItems='center' justifyContent={'center'} sx={{ width: '90%', marginLeft: '5%' }}>
                 <Divider sx={{ mt: '5%' }} />
             </Box>
-            <ImageWithDescription imgUrlFirst={generalCardsData[0].imgUrl} descriptionFirst={generalCardsData[0].description} 
-            imgUrlSecond={generalCardsData[1].imgUrl} descriptionSecond={generalCardsData[1].description}/>
+            <Typography sx={[text.textH2, { mt: '3%', ml: '5%', mr: '5%' }]}>
+                ¿Qué cubre un seguro de vida?
+            </Typography>
+            <ImageWithDescription imgUrlFirst={generalCardsData[2].imgUrl} descriptionFirst={generalCardsData[2].description}
+                imgUrlSecond={generalCardsData[3].imgUrl} descriptionSecond={generalCardsData[3].description} />
             <Footer title="Footer" />
         </ThemeProvider >
     )
